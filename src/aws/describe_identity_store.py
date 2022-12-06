fced3f276f74b6348be12fe614d757e159e8ffe1
labvel <walej@VelPro.localdomain>
2022-12-05 19:02:39 -0500

    create banner

--- src/aws/describe_identity_store.py
+++ src/aws/describe_identity_store.py
@@ -1,5 +1,6 @@
 import boto3
 from .describe_sso import list_instances, list_account_assignments, list_permissions_set, extends_permissions_set
+import logging
 
 
 def list_groups(identity_store_id, client=boto3.client('identitystore', region_name="us-east-2"), ):
@@ -63,7 +64,8 @@ def complete_group_members(group_members, users_list):
     return group_members
 
 
-def extend_account_assignments(accounts_list, permissions_sets, store_arn, client_sso=boto3.client('identitystore', region_name="us-east-2")):
+def extend_account_assignments(accounts_list, permissions_sets, store_arn,
+                               client_sso=boto3.client('identitystore', region_name="us-east-2")):
     account_assignments = []
     for p in permissions_sets:
         # TODO make for each account
@@ -71,13 +73,14 @@ def extend_account_assignments(accounts_list, permissions_sets, store_arn, clien
         for ac in accounts_list:
             assign = list_account_assignments(instance_arn=store_arn, account_id=ac["Id"], client=client_sso,
                                               permission_set_arn=p)
-            print("AccountAssignments ", assign)
+            logging.debug("AccountAssignments ", assign)
             for a in assign:
                 account_assignments.append(a)
     return account_assignments
 
 
-def add_users_and_groups_assign(account_assignments_list, user_and_group_list, user_list,list_permissions_set_arn_name):
+def add_users_and_groups_assign(account_assignments_list, user_and_group_list, user_list,
+                                list_permissions_set_arn_name):
     for a in account_assignments_list:
         for g in user_and_group_list:
             if len(a) > 0 and a['PrincipalType'] == 'GROUP' and g["group_id"] == a['PrincipalId']:
@@ -92,7 +95,7 @@ def add_users_and_groups_assign(account_assignments_list, user_and_group_list, u
                     f"Account {a['AccountId']} assign to {a['PrincipalType']} {u['UserName']} with permission set {a['PermissionSetArn']} or {list_permissions_set_arn_name[a['PermissionSetArn']]}")
                 a["UserName"] = u['UserName']
                 a["PermissionSetName"] = list_permissions_set_arn_name[a['PermissionSetArn']]
-    print(f"Account Assignments --> {account_assignments_list}")
+    logging.debug(f"Account Assignments --> {account_assignments_list}")
     return account_assignments_list
 
 
@@ -101,13 +104,9 @@ def order_accounts_assignments_list(accounts_dict, account_assignments):
     for ac in accounts_dict:
         final_account_assignments[ac["Name"]] = []
         for a in account_assignments:
-            print(a)
+
             if ac["Id"] == a["AccountId"]:
                 final_account_assignments[ac["Name"]].append(a)
-                l = len(final_account_assignments[ac["Name"]])
-                print(l)
 
-        print(f"Final Account Assignments: {final_account_assignments}")
+        logging.debug(f"Final Account Assignments: {final_account_assignments}")
     return final_account_assignments
-
-
--- src/reverse_diagrams.py
+++ src/reverse_diagrams.py
@@ -1,18 +1,22 @@
 import boto3
 import argparse
 import logging
+from colorama import Fore
 from datetime import datetime
 from aws.describe_organization import describe_organization, \
     list_accounts, index_accounts, list_roots, list_organizational_units, index_ous
 from aws.describe_identity_store import order_accounts_assignments_list, extend_account_assignments, \
     list_permissions_set, list_groups, get_members, list_users, complete_group_members, add_users_and_groups_assign
-from aws.describe_sso import list_instances, list_account_assignments, extends_permissions_set
+from aws.describe_sso import list_instances, extends_permissions_set
 from dgms.graph_mapper import create_mapper, create_sso_mapper_complete, create_sso_mapper
+from banner.banner import get_version
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main() -> int:
     """
-    Validate SCP policies using AWS Access Analyzer API and create reports
+    Crete sso mapper and organization diagram from your current state
     :return:
     """
     print('Compliance date:', datetime.now())
@@ -25,6 +29,8 @@ def main() -> int:
                         help="Set if you want to create graph for your organization", action='store_true')
     parser.add_argument("-i", "--graph_identity",
                         help="Set if you want to create graph for your IAM Center", action='store_true')
+    parser.add_argument("-v", "--version",
+                        help="Show version", action='store_true')
 
     # Read arguments from command line
     args = parser.parse_args()
@@ -39,51 +45,56 @@ def main() -> int:
     if args.graph_organization:
         client_org = boto3.client('organizations')
         organization = describe_organization(client_org)
-        print("The Organization info")
-        print(organization)
-        print("The Roots Info")
+        print(Fore.BLUE + "🔄 Getting Organization Info" + Fore.RESET)
+        logging.debug(organization)
+        logging.debug("The Roots Info")
         roots = list_roots(client=client_org)
-        print(roots)
-        print("The Organizational Units list ")
+        logging.debug(roots)
+        print(Fore.BLUE + "🔄 The Organizational Units list " + Fore.RESET)
+        logging.debug("The Organizational Units list ")
         ous = list_organizational_units(parent_id=roots[0]["Id"], client=client_org)
-        print(ous)
-        print("The Organizational Units list with parents info")
+        logging.debug(ous)
+        logging.debug("The Organizational Units list with parents info")
         i_ous = index_ous(ous, client=client_org)
-        print(i_ous)
-        print("The Account list info")
+        logging.debug(i_ous)
+        print(Fore.BLUE + "🔄 Getting the Account list info" + Fore.RESET)
         l_accounts = list_accounts(client_org)
-        print(l_accounts)
-        print("The Account list with parents info")
+        logging.debug(l_accounts)
+        logging.debug("The Account list with parents info")
         i_accounts = index_accounts(l_accounts)
-        print(i_accounts)
+        logging.debug(i_accounts)
 
         create_mapper(template_file="graph_org.py", org=organization, root_id=roots[0]["Id"], list_ous=ous,
                       list_accounts=i_accounts)
+
+        print(Fore.YELLOW + "Run -> python3 graph_org.py " + Fore.RESET)
+
     if args.graph_identity:
         # boto3.setup_default_session(profile_name='labvel-master', region_name="us-east-2")
         client_identity = boto3.client('identitystore', region_name="us-east-2")
         client_sso = boto3.client('sso-admin', region_name="us-east-2")
 
         store_instances = list_instances(client=client_sso)
-        print(store_instances)
+        print(Fore.BLUE + "🔄 Getting Identity store instance info" + Fore.RESET)
+        logging.debug(store_instances)
         store_id = store_instances[0]["IdentityStoreId"]
         store_arn = store_instances[0]["InstanceArn"]
-        print("list groups")
+        print(Fore.BLUE + "List groups" + Fore.RESET)
         l_groups = list_groups(store_id, client=client_identity)
-        print(l_groups)
+        logging.debug(l_groups)
 
-        print("Get member group")
+        print(Fore.BLUE + "🔄Get groups and Users info" + Fore.RESET)
 
         m_groups = get_members(store_id, l_groups, client=client_identity)
 
-        print(m_groups)
+        logging.debug(m_groups)
 
-        print("Extend Group Members")
+        logging.debug("Extend Group Members")
         l_users = list_users(store_id, client=client_identity)
-        print(l_users)
+        logging.debug(l_users)
         c_users_and_groups = complete_group_members(m_groups, l_users)
 
-        print(c_users_and_groups)
+        logging.debug(c_users_and_groups)
         # Get Account assingments
         permissions_set = list_permissions_set(instance_arn=store_arn, client=client_sso)
         l_permissions_set_arn_name = extends_permissions_set(permissions_sets=permissions_set, store_arn=store_arn,
@@ -100,10 +111,13 @@ def main() -> int:
                                                           user_and_group_list=c_users_and_groups,
                                                           user_list=l_users,
                                                           list_permissions_set_arn_name=l_permissions_set_arn_name)
-
-        f_accounts= order_accounts_assignments_list(accounts_dict=l_accounts, account_assignments=account_assignments)
+        print(Fore.BLUE + "🔄 Getting account assignments, users and groups" + Fore.RESET)
+        f_accounts = order_accounts_assignments_list(accounts_dict=l_accounts, account_assignments=account_assignments)
         create_sso_mapper_complete(template_file="graph_sso_complete.py", acc_assignments=f_accounts)
-        create_sso_mapper(template_file="graph_sso.py", group_and_members= c_users_and_groups)
+        create_sso_mapper(template_file="graph_sso.py", group_and_members=c_users_and_groups)
+        print(Fore.YELLOW + "Run -> python3 graph_sso_complete.py " + Fore.RESET)
+    if args.version:
+        get_version()
 
 
 if __name__ == '__main__':
